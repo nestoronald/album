a37d7867eea8af9dfd3f503b005a473375a1e76c
Ronald_Adiestra <ronald4261@hotmail.com>
2014-06-25 03:35:53 -0500

    crud 01 A

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -6,6 +6,12 @@ namespace Album;
 
  use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
  use Zend\ModuleManager\Feature\ConfigProviderInterface;
+ // Add these import statements:
+ use Album\Model\Album;
+ use Album\Model\AlbumTable;
+ use Zend\Db\ResultSet\ResultSet;
+ use Zend\Db\TableGateway\TableGateway;
+
 
  class Module implements AutoloaderProviderInterface, ConfigProviderInterface
  {
@@ -22,11 +28,12 @@ namespace Album;
              ),
          );
      }
-     /*ruta de las opciones de configuracion del módulo*/
+
      public function getConfig()
      {
          return include __DIR__ . '/config/module.config.php';
      }
+// Add this method:     
      public function getServiceConfig()
      {
          return array(
@@ -45,4 +52,5 @@ namespace Album;
              ),
          );
      }
+
  }
